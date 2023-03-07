85785b397294966f332c4a65ff9b7d71a2f595c0
le.khoi <khoilecontact@gmail.com>
2023-03-07 09:36:07 +0700

    Passing data between components

--- App.tsx
+++ App.tsx
@@ -1,11 +1,36 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
+import React, {useState} from 'react';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <ParentComponent/>
       <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const ParentComponent = () => {
+  const [name, setName] = useState('KhoiLe');
+
+  const handleNameChanged = (newName) => {
+    setName(newName);
+  };
+
+  return(
+    <View>
+      <ChildComponent name={name} onNameChanged={handleNameChanged}/>
+      <Text>My name is {name}</Text>     
+    </View>
+  );
+}
+
+const ChildComponent = (props) => {
+  return (
+    <View>
+      <Text>Enter your name: </Text>
+      <TextInput style = {styles.textInput} value={props.name} onChangeText={props.onNameChanged} />
     </View>
   );
 }
@@ -16,5 +41,12 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  textInput: {
+    height: 40, 
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+    borderRadius: 10
   },
 });
