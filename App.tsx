b31b05218255c474ec2417c87b764c8dec92ca7a
le.khoi <khoilecontact@gmail.com>
2023-03-11 11:28:43 +0700

    Using props

--- App.tsx
+++ App.tsx
@@ -1,13 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
+      <Hello name="LeKhoi"/>
       <StatusBar style="auto" />
     </View>
   );
+}
+
+const Hello = (props) => {
+  return (
+    <SafeAreaView>
+      <Text>Good morning {props.name}</Text>
+      <Text>How is {props.name} doing today</Text>
+    </SafeAreaView>
+  )
 }
 
 const styles = StyleSheet.create({
