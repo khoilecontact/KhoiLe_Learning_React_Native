3db88ce42f2ffa8e7108993aed8250811e17af5f
le.khoi <khoilecontact@gmail.com>
2023-03-15 15:06:14 +0700

    Using TextInput

--- App.tsx
+++ App.tsx
@@ -1,10 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
 
 export default function App() {
+  const [inputText, setText] = useState('');
+
+  const handleTextInput = (text) => {
+    setText(text);
+  }
+
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
+      <TextInput
+      style = {styles.input}
+      onChangeText={handleTextInput}
+      value={inputText}
+      placeholder='Type something'
+      />
+      <Text>You just type {inputText}</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -16,5 +30,12 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: '#ccc',
+    borderRadius: 5,
+    padding: 10,
+    width: '80%',
   },
 });
