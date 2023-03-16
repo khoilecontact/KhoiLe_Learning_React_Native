8482f9a7f0447c0250c98f922cad33fe6c73efaf
le.khoi <khoilecontact@gmail.com>
2023-03-16 09:24:13 +0700

    Using FlatList

--- App.tsx
+++ App.tsx
@@ -1,12 +1,33 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { FlatList, StyleSheet, Text, View, SafeAreaView } from 'react-native';
 
 export default function App() {
+  const data = [
+    { id: '1', name: 'Item 1' },
+    { id: '2', name: 'Item 2' },
+    { id: '3', name: 'Item 3' },
+    { id: '4', name: 'Item 4' },
+    { id: '5', name: 'Item 5' },
+    { id: '6', name: 'Item 6' },
+    { id: '7', name: 'Item 7' },
+  ];
+
+  const renderItem = ({ item }) => {
+    return <Text style={styles.text}>{item.name}</Text>
+  }
+
+  const Separator = () => <View style={styles.separator} />;
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+    <SafeAreaView style={styles.container}>
+      <FlatList
+        data={data}
+        renderItem={renderItem}
+        ItemSeparatorComponent={Separator}
+        keyExtractor={(item) => item.id}
+      />
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
@@ -16,5 +37,15 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  text: {
+    fontSize: 20,
+    borderWidth: 2,
+    padding: 10,
+    borderRadius: 10,
+  },
+  separator: {
+    height: 50,
+    backgroundColor: '#fff',
   },
 });
